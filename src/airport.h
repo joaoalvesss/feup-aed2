f10aac7102490cda53fde90a5c6a281c44d482c2
antoniorama <92859799+tonirama@users.noreply.github.com>
2022-12-27 19:31:13 +0000

    added airport and flight getters

--- src/airport.h
+++ src/airport.h
@@ -1,4 +1,23 @@
-#ifndef PROJETO_AED_2_AIRPORT_H
-#define PROJETO_AED_2_AIRPORT_H
+#ifndef PROJETO_AED_2_FLIGHT_H
+#define PROJETO_AED_2_FLIGHT_H
 
-#endif //PROJETO_AED_2_AIRPORT_H
+#include <sstream>
+
+class Flight {
+    std::string sourceAirportCode, targetAirportCode, airlineCode;
+
+public:
+    const std::string& getSourceAirportCode() const {
+        return sourceAirportCode;
+    }
+
+    const std::string& getTargetAirportCode() const {
+        return targetAirportCode;
+    }
+
+    const std::string& getAirlineCode() const {
+        return airlineCode;
+    }
+};
+
+#endif //PROJETO_AED_2_FLIGHT_H
--- src/flight.h
+++ src/flight.h
@@ -1,4 +1,23 @@
 #ifndef PROJETO_AED_2_FLIGHT_H
 #define PROJETO_AED_2_FLIGHT_H
 
+#include <sstream>
+
+class Flight {
+    std::string sourceAirportCode, targetAirportCode, airlineCode;
+
+public:
+    const std::string& getSourceAirportCode() const {
+        return sourceAirportCode;
+    }
+
+    const std::string& getTargetAirportCode() const {
+        return targetAirportCode;
+    }
+
+    const std::string& getAirlineCode() const {
+        return airlineCode;
+    }
+};
+
 #endif //PROJETO_AED_2_FLIGHT_H
